52a5b8662f4d78d9018b4bc3d95cc1288d879cdc
lensflare@freenet.de <lensflare@freenet.de@3883ae20-0a2c-a88d-41a9-1bc2c5058465>
2011-09-14 21:24:48 +0000

    improved camera rotation state

--- trunk/Lensflare XNA Lib/Lensflare XNA Lib/src/Util/Camera.cs
+++ trunk/Lensflare XNA Lib/Lensflare XNA Lib/src/Util/Camera.cs
@@ -5,14 +5,12 @@ namespace Util {
         protected float aspectRatio = 1.0f;
         protected Vector3 position;
         protected Vector3 rotation = Vector3.Zero;
-        protected Matrix rotationMatrix = Matrix.Identity;
         protected Matrix view = Matrix.Identity;
         protected Matrix projection = Matrix.Identity;
 
         public float AspectRatio { get { return aspectRatio; } set { aspectRatio = value; } }
         public Vector3 Position { get { return position; } set { position = value; } }
         public Vector3 Rotation { get { return rotation; } set { rotation = value; } }
-        public Matrix RotationMatrix { get { return rotationMatrix; } set { rotationMatrix = value; } }
         public Matrix ViewMatrix { get { return view; } set { view = value; } }
         public Matrix ProjectionMatrix { get { return projection; } set { projection = value; } }
         public Vector3 UpVector { get { return Vector3.Up; } }
@@ -20,8 +18,12 @@ namespace Util {
         public Camera() {
         }
 
+        public Matrix CalcRotationMatrix() {
+            return Matrix.CreateRotationX(rotation.X) * Matrix.CreateRotationY(rotation.Y);
+        }
+
         public virtual void Update(GameTime gameTime) {
-            rotationMatrix = Matrix.CreateRotationX(rotation.X) * Matrix.CreateRotationY(rotation.Y);
+            Matrix rotationMatrix = CalcRotationMatrix();
 
             Vector3 originalTarget = new Vector3(0, 0, -1);
             Vector3 rotatedTarget = Vector3.Transform(originalTarget, rotationMatrix);
