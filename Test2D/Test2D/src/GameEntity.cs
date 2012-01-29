4dc2dfe676a7e61bcfdbb3ed6b7de10a88ed4592
lensflare <lensflare@freenet.de>
2012-01-29 23:36:07 +0000

    added a test entity

---
+++ Test2D/Test2D/src/GameEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LensflareGameFramework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace Test2D {
+    abstract class GameEntity : Entity {
+        public Game2D Game { get; private set; }
+
+        public GameEntity(Game2D game) {
+            Game = game;
+        }
+
+        protected override void Dispose() {
+            Game = null;
+        }
+    }
+}
