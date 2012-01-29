849dd70e2acac79dafb61d8d8cf25ca11dbc3ef6
lensflare <lensflare@freenet.de>
2012-01-29 18:05:17 +0000

    fixed project

---
+++ Lensflare XNA Lib/Lensflare XNA Lib/src/Util/ProceduralTexture.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System.Diagnostics;
+
+namespace Util {
+    public class ProceduralTexture {
+        protected GraphicsDevice g;
+
+        public ProceduralTexture(GraphicsDevice g) {
+            this.g = g;
+        }
+
+        public Texture2D ColorToTexture(Color color) {
+            Texture2D texture = new Texture2D(g, 1, 1);
+            texture.SetData<Color>(new Color[] { color });
+            return texture;
+        }
+
+        public Texture2D BitmapToTexture(ref Color[,] colors) {
+            int width = colors.GetLength(0);
+            int height = colors.GetLength(1);
+            Texture2D texture = new Texture2D(g, width, height);
+            Color[] colors1D = new Color[width*height];
+            int i = 0;
+            for (int y = 0; y < height; ++y) {
+                for (int x = 0; x < width; ++x) {
+                    colors1D[i++] = colors[x,y];
+                }
+            }
+            Debug.Assert(i == colors1D.Length);
+            texture.SetData(colors1D);
+            return texture;
+        }
+
+        public Texture2D Test(int width, int height) { //TODO: ...
+            Texture2D texture = new Texture2D(g, width, height);
+            Color[] colors = new Color[width * height];
+            for (int y = 0; y < height; ++y) {
+                for (int x = 0; x < width; ++x) {
+                    float fx = x/(float)(Math.PI*2)/16;
+                    float fy = y/(float)(Math.PI*2)/16;
+                    float red = 0.5f*((float)Math.Sin(fx*fy)+1.0f);
+                    float green = 0.5f * ((float)-Math.Sin(fx * fy) + 1.0f);
+                    float blue = 0;
+                    colors[x + y*width] = new Color(red, green, blue);
+                }
+            }
+            texture.SetData(colors);
+            return texture;
+        }
+    }
+}
