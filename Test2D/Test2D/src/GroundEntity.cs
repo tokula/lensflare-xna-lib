439a6950d8de326489b1287fd39fd30fce8a6fc6
lensflare <lensflare@freenet.de>
2012-02-08 22:43:27 +0000

    added procedural cell map generation

--- Test2D/Test2D/src/GroundEntity.cs
+++ Test2D/Test2D/src/GroundEntity.cs
@@ -10,15 +10,27 @@ using FarseerPhysics.Collision.Shapes;
 namespace Test2D {
     class GroundEntity : GameEntity {
         Body body;
-        EdgeShape shape;
-        Fixture fixture;
+        //EdgeShape shape;
+        LinkedList<EdgeShape> lineShapes = new LinkedList<EdgeShape>();
 
         public GroundEntity(Game2D game) : base(game) {
             body = new Body(game.world);
             body.BodyType = BodyType.Static;
 
-            shape = new EdgeShape(new Vector2(-200, 100), new Vector2(300, 150));
-            fixture = new Fixture(body, shape);
+            //shape = new EdgeShape(new Vector2(-200, 100), new Vector2(300, 150));
+            //new Fixture(body, shape);
+
+            CellMapBuilder cmb = new CellMapBuilder(Game.random);
+            float[] components = cmb.Test(16, 16, 200);
+            for (int i = 0; i < components.Length; i += 4) {
+                float x1 = components[i];
+                float y1 = components[i + 1];
+                float x2 = components[i + 2];
+                float y2 = components[i + 3];
+                EdgeShape lineShape = new EdgeShape(new Vector2(x1, y1), new Vector2(x2, y2));
+                new Fixture(body, lineShape);
+                lineShapes.AddLast(lineShape);
+            }
         }
 
         protected override void Dispose() {
@@ -31,7 +43,10 @@ namespace Test2D {
 
         public override void Draw() {
             Vector2 screenPosition = Game.camera.PositionScreen - Game.camera.PositionWorld;
-            Primitive2.DrawLine(Game.spriteBatch, screenPosition + shape.Vertex1, screenPosition + shape.Vertex2, Color.DarkGreen);
+            foreach(EdgeShape shape in lineShapes) {
+                Primitive2.DrawLine(Game.spriteBatch, screenPosition + shape.Vertex1, screenPosition + shape.Vertex2, Color.Yellow);
+            }
+            //Primitive2.DrawLine(Game.spriteBatch, screenPosition + shape.Vertex1, screenPosition + shape.Vertex2, Color.DarkGreen);
         }
     }
 }
