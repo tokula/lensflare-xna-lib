5c7ca1b07e143afef48f472c125e70761d1ad61f
lensflare@freenet.de <lensflare@freenet.de@3883ae20-0a2c-a88d-41a9-1bc2c5058465>
2012-02-12 22:15:48 +0000

    implemented frustum culling

--- trunk/Lensflare XNA Lib/Lensflare XNA Lib/src/Camera/Camera2.cs
+++ trunk/Lensflare XNA Lib/Lensflare XNA Lib/src/Camera/Camera2.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Camera {
     public class Camera2 {
@@ -19,6 +20,36 @@ namespace Camera {
 
         public virtual void Update(GameTime gameTime) {
 
+        }
+
+        public bool IsTextureVisible(Texture2D texture, Vector2 position) {
+            Vector2 v = positionScreen - size * 0.5f;
+            float cx1 = v.X;
+            float cy1 = v.Y;
+            float cx2 = cx1 + size.X;
+            float cy2 = cy1 + size.Y;
+            
+            float ex1 = position.X;
+            float ey1 = position.Y;
+            float ex2 = ex1 + texture.Width;
+            float ey2 = ey1 + texture.Height;
+
+            return !(ex2 < cx1 || ex1 > cx2 || ey2 < cy1 || ey1 > cy2);
+        }
+
+        public bool IsLineVisible(Vector2 point1, Vector2 point2) {
+            Vector2 v = positionScreen - size * 0.5f;
+            float cx1 = v.X;
+            float cy1 = v.Y;
+            float cx2 = cx1 + size.X;
+            float cy2 = cy1 + size.Y;
+
+            float ex1 = point1.X;
+            float ey1 = point1.Y;
+            float ex2 = point2.X;
+            float ey2 = point2.Y;
+
+            return (ex1 >= cx1 && ex1 <= cx2 && ey1 >= cy1 && ey1 <= cy2) || (ex2 >= cx1 && ex2 <= cx2 && ey2 >= cy1 && ey2 <= cy2);
         }
     }
 }
