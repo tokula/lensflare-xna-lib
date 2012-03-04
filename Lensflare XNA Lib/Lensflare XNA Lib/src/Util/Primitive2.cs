5f6231807dd689d02ce46cf6e8aab7f79465a7f5
lensflare <lensflare@freenet.de>
2012-03-04 19:33:37 +0000

    -

--- Lensflare XNA Lib/Lensflare XNA Lib/src/Util/Primitive2.cs
+++ Lensflare XNA Lib/Lensflare XNA Lib/src/Util/Primitive2.cs
@@ -13,17 +13,14 @@ namespace Util {
         }
 
         public static void DrawPixel(SpriteBatch spriteBatch, Vector2 position, Color color, float layerDepth) {
-            //spriteBatch.Draw(pixel, position, color);
             spriteBatch.Draw(pixel, position, null, color, 0, Vector2.Zero, 1, SpriteEffects.None, layerDepth);
         }
 
         public static void DrawHorizontalLine(SpriteBatch spriteBatch, Vector2 position, float length, Color color, float layerDepth) {
-            //spriteBatch.Draw(pixel, new Rectangle((int)position.X, (int)position.Y, (int)length, 1), color);
             spriteBatch.Draw(pixel, new Rectangle((int)position.X, (int)position.Y, (int)length, 1), null, color, 0, Vector2.Zero, SpriteEffects.None, layerDepth);
         }
 
         public static void DrawVerticalLine(SpriteBatch spriteBatch, Vector2 position, float length, Color color, float layerDepth) {
-            //spriteBatch.Draw(pixel, new Rectangle((int)position.X, (int)position.Y, 1, (int)length), color);
             spriteBatch.Draw(pixel, new Rectangle((int)position.X, (int)position.Y, 1, (int)length), null, color, 0, Vector2.Zero, SpriteEffects.None, layerDepth);
         }
 
@@ -34,7 +31,6 @@ namespace Util {
         }
 
         public static void DrawTextureLine(SpriteBatch spriteBatch, Texture2D texture, Vector2 start, Vector2 end, float thickness, Color color, float layerDepth) {
-            //int distance = (int)Vector2.Distance(start, end);
             float distance = Vector2.Distance(start, end);
             float rotationAngle = (float)Math.Atan2(end.Y - start.Y, end.X - start.X);
             spriteBatch.Draw(texture, start, null, color, rotationAngle, new Vector2(0, 0.5f * texture.Height), new Vector2(distance / texture.Width, thickness / texture.Height), SpriteEffects.None, layerDepth);
@@ -48,7 +44,6 @@ namespace Util {
                 DrawVerticalLine(spriteBatch, new Vector2(position.X, position.Y + 1), size.Y - 2, color, layerDepth);
                 DrawVerticalLine(spriteBatch, new Vector2(position.X + size.X - 1, position.Y + 1), size.Y - 2, color, layerDepth);
             } else {
-                //spriteBatch.Draw(pixel, new Rectangle((int)position.X, (int)position.Y, (int)size.X, (int)size.Y), color);
                 spriteBatch.Draw(pixel, new Rectangle((int)position.X, (int)position.Y, (int)size.X, (int)size.Y), null, color, 0, Vector2.Zero, SpriteEffects.None, layerDepth);
             }
         }
@@ -59,14 +54,14 @@ namespace Util {
                 for (int p = 0; p < a; ++p) {
                     int e = (int)Math.Sqrt(r * r - p * p);
 
-                    spriteBatch.Draw(pixel, position + new Vector2(p, e), null, color, 0, Vector2.Zero, 1, SpriteEffects.None, layerDepth);
-                    spriteBatch.Draw(pixel, position + new Vector2(p, -e), null, color, 0, Vector2.Zero, 1, SpriteEffects.None, layerDepth);
-                    spriteBatch.Draw(pixel, position + new Vector2(e, p), null, color, 0, Vector2.Zero, 1, SpriteEffects.None, layerDepth);
-                    spriteBatch.Draw(pixel, position + new Vector2(-e, p), null, color, 0, Vector2.Zero, 1, SpriteEffects.None, layerDepth);
+                    spriteBatch.Draw(pixel, position + new Vector2( p,  e), null, color, 0, Vector2.Zero, 1, SpriteEffects.None, layerDepth);
+                    spriteBatch.Draw(pixel, position + new Vector2( p, -e), null, color, 0, Vector2.Zero, 1, SpriteEffects.None, layerDepth);
+                    spriteBatch.Draw(pixel, position + new Vector2( e,  p), null, color, 0, Vector2.Zero, 1, SpriteEffects.None, layerDepth);
+                    spriteBatch.Draw(pixel, position + new Vector2(-e,  p), null, color, 0, Vector2.Zero, 1, SpriteEffects.None, layerDepth);
 
-                    spriteBatch.Draw(pixel, position + new Vector2(-p, e), null, color, 0, Vector2.Zero, 1, SpriteEffects.None, layerDepth);
+                    spriteBatch.Draw(pixel, position + new Vector2(-p,  e), null, color, 0, Vector2.Zero, 1, SpriteEffects.None, layerDepth);
                     spriteBatch.Draw(pixel, position + new Vector2(-p, -e), null, color, 0, Vector2.Zero, 1, SpriteEffects.None, layerDepth);
-                    spriteBatch.Draw(pixel, position + new Vector2(e, -p), null, color, 0, Vector2.Zero, 1, SpriteEffects.None, layerDepth);
+                    spriteBatch.Draw(pixel, position + new Vector2( e, -p), null, color, 0, Vector2.Zero, 1, SpriteEffects.None, layerDepth);
                     spriteBatch.Draw(pixel, position + new Vector2(-e, -p), null, color, 0, Vector2.Zero, 1, SpriteEffects.None, layerDepth);
                 }
             } else {
@@ -74,10 +69,8 @@ namespace Util {
                 for (int p = 0; p < a; ++p) {
                     int e = (int)Math.Sqrt(r * r - p * p);
                     if (p > 0) {
-                        //spriteBatch.Draw(pixel, new Rectangle((int)position.X + p, (int)position.Y - e, 1, e * 2 + 1), color);
                         spriteBatch.Draw(pixel, new Rectangle((int)position.X + p, (int)position.Y - e, 1, e * 2 + 1), null, color, 0, Vector2.Zero, SpriteEffects.None, layerDepth);
                     }
-                    //spriteBatch.Draw(pixel, new Rectangle((int)position.X - p, (int)position.Y - e, 1, e * 2 + 1), color);
                     spriteBatch.Draw(pixel, new Rectangle((int)position.X - p, (int)position.Y - e, 1, e * 2 + 1), null, color, 0, Vector2.Zero, SpriteEffects.None, layerDepth);
                 }
             }
