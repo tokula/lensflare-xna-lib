9ae50c533167b9e70c394504475e4666e5563586
lensflare <lensflare@freenet.de>
2012-11-09 20:35:38 +0000

    added dist calculation for IntVector2

--- Lensflare XNA Lib/Lensflare XNA Lib/src/Util/IntVector2.cs
+++ Lensflare XNA Lib/Lensflare XNA Lib/src/Util/IntVector2.cs
@@ -69,5 +69,17 @@ namespace Util {
         public static Vector2 operator *(IntVector2 v, float scale) {
             return new Vector2(v.X * scale, v.Y * scale);
         }
+
+        public static float Distance(IntVector2 v1, IntVector2 v2) {
+            int dx = v1.X - v2.X;
+            int dy = v1.Y - v2.Y;
+            return (float)Math.Sqrt(dx * dx + dy * dy);
+        }
+
+        public static void Distance(ref IntVector2 v1, ref IntVector2 v2, out float result) {
+            int dx = v1.X - v2.X;
+            int dy = v1.Y - v2.Y;
+            result = (float)Math.Sqrt(dx * dx + dy * dy);
+        }
     }
 }
