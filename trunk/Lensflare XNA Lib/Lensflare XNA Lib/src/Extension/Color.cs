265cb25be59005c863ba78f283f726b0d1b05bef
lensflare@freenet.de <lensflare@freenet.de@3883ae20-0a2c-a88d-41a9-1bc2c5058465>
2012-11-21 01:03:18 +0000

    added Color extension; added vertical gradient texture generation;

---
+++ trunk/Lensflare XNA Lib/Lensflare XNA Lib/src/Extension/Color.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Util {
+    public static class ColorExtension {
+        public static Color Sum(this Color color1, Color color2) {
+            return new Color(color1.ToVector4() + color2.ToVector4());
+        }
+    }
+}
